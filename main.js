28d60af37fe8f83ae4c7973f9817f7634c2291ed
gccbs01002 <44439957+gccbs01002@users.noreply.github.com>
2025-09-01 02:05:52 +0800

    0901/Star (#4)
    * change-html commit
    
    * change commit
    
    0901/Star
    
    1.barbie-info.html更換位置到recom-excit.html
    2.新增本頁專屬header-recom-excit.ejs
    3.components套件
    4.對應3.的slide-overlay.js、excite-gallery.js

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
-import './assets/scss/all.scss';
-import 'bootstrap/dist/js/bootstrap.min.js';
+import "./assets/scss/all.scss";
+import "bootstrap/dist/js/bootstrap.min.js";
 
-console.log('Hello world');
-
+console.log("Hello world");
 
 const BASE = import.meta.env.BASE_URL; // dev: '/', prod: '/Moodie/'
-link.href = BASE + 'pages/barbie-info.html';
+link.href = BASE + "pages/recommend/recom-excit.html";
+
+export default defineConfig({
+  base: "/Moodie/",
+  server: {
+    open: "/Moodie/pages/recommend/recom-excit.html", // 換成你要的頁
+  },
+  // build.rollupOptions.input 只影響 build，不影響 dev
+});
