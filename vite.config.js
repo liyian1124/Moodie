bd50a949b27a50b8f0a4b2b922278292911e9a40
star <gccbs01002@gmail.com>
2025-09-01 23:45:10 +0800

    build commit

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,20 @@
-import { defineConfig } from 'vite';
-import { ViteEjsPlugin } from 'vite-plugin-ejs';
-import { fileURLToPath } from 'node:url';
-import path from 'node:path';
-import { glob } from 'glob';
-
-import liveReload from 'vite-plugin-live-reload';
+import { defineConfig } from "vite";
+import { ViteEjsPlugin } from "vite-plugin-ejs";
+import { fileURLToPath } from "node:url";
+import path from "node:path";
+import { glob } from "glob";
+import { resolve } from "path";
+import liveReload from "vite-plugin-live-reload";
 
 function moveOutputPlugin() {
   return {
-    name: 'move-output',
-    enforce: 'post',
-    apply: 'build',
+    name: "move-output",
+    enforce: "post",
+    apply: "build",
     async generateBundle(options, bundle) {
       for (const fileName in bundle) {
-        if (fileName.startsWith('pages/')) {
-          const newFileName = fileName.slice('pages/'.length);
+        if (fileName.startsWith("pages/")) {
+          const newFileName = fileName.slice("pages/".length);
           bundle[fileName].fileName = newFileName;
         }
       }
@@ -26,31 +26,38 @@ export default defineConfig({
   // base 的寫法:
   // base: '/Repository 的名稱/'
   base: '/Moodie/',
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        excite: resolve(__dirname, "pages/roles/excitement.html"),
+        recomExcit: resolve(__dirname, "pages/recommend/recom-excit.html"),
+      },
+    },
+  },
   plugins: [
-    liveReload(['./layout/**/*.ejs', './pages/**/*.ejs', './pages/**/*.html']),
+    liveReload(["./layout/**/*.ejs", "./pages/**/*.ejs", "./pages/**/*.html"]),
     ViteEjsPlugin(),
     moveOutputPlugin(),
   ],
   server: {
     // 啟動 server 時預設開啟的頁面
-    open: 'pages/movie_info/barbie-info.html',
+    open: "pages/movie_info/barbie-info.html",
   },
   build: {
     rollupOptions: {
       input: Object.fromEntries(
         glob
-          .sync('pages/**/*.html')
+          .sync("pages/**/*.html")
           .map((file) => [
             path.relative(
-              'pages',
+              "pages",
               file.slice(0, file.length - path.extname(file).length)
             ),
             fileURLToPath(new URL(file, import.meta.url)),
           ])
       ),
     },
-    outDir: 'dist',
+    outDir: "dist",
   },
 });
-
-
